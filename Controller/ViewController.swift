bc9880f0d57fc4f146aa759fbcbaacb21abd11e7
Faiq Ali <faiqueali017@gmail.com>
2021-03-23 23:48:34 +0500

    Code documentation added

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -28,13 +28,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //MARK:- ImagePicker Delegates Methods
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
+        //1- Picking or capturing image
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
             
+            //2- Converting it into CIImage
             guard let ciimage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert to CIImage")
             }
             
+            //3- Passing image for detecting
             detect(image: ciimage)
         }
         
@@ -43,10 +46,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     //MARK:- Helper Methods
     func detect(image: CIImage) {
+        //1- Load up our Inceptionv3 Model
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
             fatalError("Loading CoreML Model Fails")
         }
         
+        //2- Create a request for classification from model
         let request = VNCoreMLRequest(model: model) { (request, error) in
             guard let request = request.results as? [VNClassificationObservation] else {
                 fatalError("Model failed to process image")
@@ -55,8 +60,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(request)
         }
         
+        //3- Data which we are passing for classification
         let handler = VNImageRequestHandler(ciImage: image)
         
+        //4- Perform the req
         do {
             try handler.perform([request])
         } catch {
